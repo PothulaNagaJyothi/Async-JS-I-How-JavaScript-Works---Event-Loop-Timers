c7c2774d14128968ac2c18b437df68b3a98f0e95
PothulaNagaJyothi <pothulanagajyothi2@gmail.com>
2025-11-26 22:34:36 +0530

    added q11.js file

---
+++ q11.js
@@ -0,0 +1,15 @@
+function task1() {
+  console.log("Task 1");
+}
+
+function task2() {
+  console.log("Task 2");
+}
+
+function task3() {
+  console.log("Task 3");
+}
+
+task1();
+task2();
+task3();
